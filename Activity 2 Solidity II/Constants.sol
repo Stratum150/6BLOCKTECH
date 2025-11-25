9447d09339a0a99a0f5a687455a7ccea59943ddc
Stratum150 <warriordan78@gmail.com>
2025-11-25 03:55:34 -0700

    Add files via upload

---
+++ Activity 2 Solidity II/Constants.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Constants {
+//coding convention to uppercase constant variables
+address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+uint public constant MY_UNIT = 123;
+}
+
+contract VAR {
+address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
